c0bf1d8ddc3a0db9c738808498793eed775ee7c1
Anirban Saha <anirban.saha@inmobi.com>
2012-07-11 06:18:37 +0000

    - Populated metadata files

--- cookbooks/inmobi_cloud/metadata.rb
+++ cookbooks/inmobi_cloud/metadata.rb
@@ -1,6 +1,16 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "Anirban Saha"
+maintainer_email "anirban.saha@inmobi.com"
 license          "Apache 2.0"
 description      "Installs/Configures inmobi_cloud"
 
 version          "0.1"
+
+recipe "inmobi_cloud::base_os_config", "Configures basic OS files"
+recipe "inmobi_cloud::apt_repo_config", "Configures apt repo files"
+recipe "inmobi_cloud::puppet_config", "Configures puppet and related services"
+
+attribute "inmobi_cloud/rs_hostname",
+ :display_name => "Nickname of the instance",
+ :description => "The FQDN of the instance to be launched",
+ :required => "required",
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::apt_repo_config" ]
