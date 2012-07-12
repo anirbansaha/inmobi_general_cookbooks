250695737d9c8881d7fcbe44c060da6d064086f2
Anirban Saha <anirban.saha@inmobi.com>
2012-07-12 11:03:40 +0000

    modifying files

--- cookbooks/inmobi_cloud/metadata.rb
+++ cookbooks/inmobi_cloud/metadata.rb
@@ -10,33 +10,34 @@ recipe "inmobi_cloud::apt_repo_config", "Configures apt repo files"
 recipe "inmobi_cloud::puppet_config", "Configures puppet and related services"
 recipe "inmobi_cloud::dns_add_record", "Adds DNS entry in UltraDNS"
 recipe "inmobi_cloud::dns_delete_record", "Removes DNS entry for UltraDNS"
+recipe "inmobi_cloud::hostname_change_config", "Reconfigures instance on hostname change, has to be run after changing the nickname of the instance"
 
 attribute "inmobi_cloud/rs_hostname",
  :display_name => "Nickname of the instance",
  :description => "The FQDN of the instance to be launched, has to be set to the ENV variable RS_SERVER_NAME from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::apt_repo_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record" ]
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::apt_repo_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config" ]
 
 attribute "inmobi_cloud/ec2_private_ip",
  :display_name => "IP of the instance",
  :description => "The private IP of the instance to be launched, has to be set to the ENV variable PRIVATE_IP from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record" ]
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config" ]
 
 attribute "inmobi_cloud/availability_zone",
  :display_name => "Availability zone of the instance",
  :description => "The ec2 availability zone of the instance to be launched, has to be set to the ENV variable EC2_AVAILABILITY_ZONE from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::base_os_config" ]
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::hostname_change_config" ]
 
 attribute "inmobi_cloud/dns_username",
  :display_name => "UltraDNS username",
  :description => "The username required to make UltraDNS API call, has to be set to the CREDENTIAL variable UDNS_USER from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record" ]
+ :recipes => [ "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config" ]
 
 attribute "inmobi_cloud/dns_password",
  :display_name => "UltraDNS password",
  :description => "The password required to make UltraDNS API call, has to be set to the CREDENTIAL variable UDNS_PASSWD from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record" ]
+ :recipes => [ "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config" ]
