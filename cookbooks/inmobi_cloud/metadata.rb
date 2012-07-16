39086b8a124873cea699891b0211890ba0940e87
Anirban Saha <anirban.saha@inmobi.com>
2012-07-16 15:29:11 +0000

    mod files

--- cookbooks/inmobi_cloud/metadata.rb
+++ cookbooks/inmobi_cloud/metadata.rb
@@ -18,7 +18,7 @@ attribute "inmobi_cloud/rs_hostname",
  :display_name => "Nickname of the instance",
  :description => "The FQDN of the instance to be launched, has to be set to the ENV variable RS_SERVER_NAME from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::apt_repo_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config", "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::apt_repo_config", "inmobi_cloud::dns_add_record", "inmobi_cloud::dns_delete_record", "inmobi_cloud::hostname_change_config", "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
 
 attribute "inmobi_cloud/ec2_private_ip",
  :display_name => "IP of the instance",
@@ -30,7 +30,7 @@ attribute "inmobi_cloud/availability_zone",
  :display_name => "Availability zone of the instance",
  :description => "The ec2 availability zone of the instance to be launched, has to be set to the ENV variable EC2_AVAILABILITY_ZONE from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::hostname_change_config", "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::base_os_config", "inmobi_cloud::hostname_change_config", "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
 
 attribute "inmobi_cloud/dns_username",
  :display_name => "UltraDNS username",
@@ -48,22 +48,22 @@ attribute "inmobi_cloud/rscale_username",
  :display_name => "RightScale username",
  :description => "The username required to make RightScale API call, has to be set to the CREDENTIAL variable RSCALE_USER from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
 
 attribute "inmobi_cloud/rscale_password",
  :display_name => "RightScale password",
  :description => "The password required to make RightScale API call, has to be set to the CREDENTIAL variable RSCALE_PASSWD from RS dashboard inputs",
  :required => "required",
- :recipes => [ "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
 
 attribute "inmobi_cloud/number_of_volumes",
  :display_name => "Number of EBS volumes required",
  :description => "Number of EBS volumes required to be created and attached to the instance, has to be given a value of type TEXT, e.g. 4 if four EBS volumes are to be attached",
  :required => "required",
- :recipes => [ "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
 
 attribute "inmobi_cloud/size_of_volume",
  :display_name => "Size of EBS volumes required",
  :description => "Size of EBS volumes required to be created and attached to the instance, has to be given a value of type TEXT, e.g. 50 if 50G volumes are to be attached",
  :required => "required",
- :recipes => [ "inmobi_cloud::ebs_create_attach" ]
+ :recipes => [ "inmobi_cloud::ebs_create_attach", "inmobi_cloud::ebs_create_attach_test" ]
