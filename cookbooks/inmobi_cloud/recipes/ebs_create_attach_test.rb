894f215b14d5c3df7130ff9a9d856f18eb05cdcb
Anirban Saha <anirban.saha@inmobi.com>
2012-07-16 17:28:12 +0000

    mod files

--- cookbooks/inmobi_cloud/recipes/ebs_create_attach_test.rb
+++ cookbooks/inmobi_cloud/recipes/ebs_create_attach_test.rb
@@ -88,13 +88,14 @@ end
 
 first_device = ""
 disk_verify = `cat /tmp/disk`.chomp
-if disk_verify != ""
+log "#{disk_verify}"
+if disk_verify == ""
+    first_device = "sdk"
+else
     disk_verify_val = disk_verify[-1]
     new_device_val = disk_verify_val + 1
     new_device_chr = new_device_val.chr 
     first_device = "sd" + "#{new_device_chr}"
-else
-    first_device = "sdk"
 end
 log "#{first_device}"
 device_val = first_device[-1]
